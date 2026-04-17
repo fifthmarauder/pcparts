4ee34e97544af15cc629c78ab4f6762a1d35e9a6
fifthmarauder <eeshashahbaz12345@gmail.com>
2026-04-17 23:36:23 +0500

    builds initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
 import styles from "./page.module.css";
+import Builds from "../components/Builds/Builds";
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <HeroSection />
+      <Builds />
     </div>
   );
 }
